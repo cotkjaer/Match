f7a8ae3e6997c4ab6d047400254cd8ef4cfc7161
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 16:53:13 +0100

    Initial Commit

---
+++ Match/Match.h
@@ -0,0 +1,19 @@
+//
+//  Match.h
+//  Match
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Match.
+FOUNDATION_EXPORT double MatchVersionNumber;
+
+//! Project version string for Match.
+FOUNDATION_EXPORT const unsigned char MatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Match/PublicHeader.h>
+
+
